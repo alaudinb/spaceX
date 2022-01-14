6e89ca30d0736e04b2673eeceab17336990e7a42
alaudinb <63118678+alaudinb@users.noreply.github.com>
2022-01-14 10:04:47 +0100

    Update SpacexPosts.js

--- components/SpacexPosts.js
+++ components/SpacexPosts.js
@@ -3,7 +3,6 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 const SpacexPosts = ({launches}) => {
-    console.log(launches)
     return (
         <>
         {launches.map((launch) => (
